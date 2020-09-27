7dec417a0ac37ab343a7e687f29f04a77301a66e
Samiha Arya <samihaarya06@gmail.com>
2020-09-27 21:01:05 +0530

    initial code structure

---
+++ src/test/java/com/udacity_course/dependency_injection/DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity_course.dependency_injection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
